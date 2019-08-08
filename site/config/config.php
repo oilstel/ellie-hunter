016d642f0c1deb8fab0a76dd338b9e8143b7168e
Elliott Cost <elliottcost@gmail.com>
2019-08-08 11:21:36 -0400

    config

--- site/config/config.php
+++ site/config/config.php
@@ -1,6 +1,6 @@
 <?php
-return [
-    'debug'  => true
-];
+// return [
+//     'debug'  => true
+// ];
 
 ?>
--- site/snippets/nav.php
+++ site/snippets/nav.php
@@ -4,7 +4,7 @@
 <?php foreach($site->children()->listed()->filterBy('template', 'in', ['home', 'default'])->limit(8) as $item): ?>
     <a <?php e($item->isActive(), ' class="active"') ?> href="<?php echo $item->url() ?>"></a>
 <?php endforeach ?>
-<? $archive = site()->children()->find('archive') ?>
-<a <?php if ($page->id() == 'archive'): ?>class="active"<?php endif ?> href="<?php echo $archive->url() ?>"></a>
+<? $archive = site()->children()->find('info') ?>
+<a <?php if ($page->id() == 'info'): ?>class="active"<?php endif ?> href="<?php echo $archive->url() ?>"></a>
 </nav>
 <?php endif ?>
