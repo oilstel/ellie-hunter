7b2667936e0e4b33cba21406dfe121dc0a283bb4
Elliott Cost <elliottcost@gmail.com>
2019-08-08 11:52:47 -0400

    archive

--- site/snippets/nav.php
+++ site/snippets/nav.php
@@ -4,7 +4,10 @@
 <?php foreach($site->children()->listed()->filterBy('template', 'in', ['home', 'default'])->limit(8) as $item): ?>
     <a <?php e($item->isActive(), ' class="active"') ?> href="<?php echo $item->url() ?>"></a>
 <?php endforeach ?>
-<? $archive = site()->children()->find('info') ?>
-<a <?php if ($page->id() == 'info'): ?>class="active"<?php endif ?> href="<?php echo $archive->url() ?>"></a>
+
+<?php foreach($site->children()->listed()->filterBy('template', 'in', ['archive']) as $item): ?>
+    <a <?php e($item->isActive(), ' class="active"') ?> href="<?php echo $item->url() ?>"></a>
+<?php endforeach ?>
+
 </nav>
 <?php endif ?>
