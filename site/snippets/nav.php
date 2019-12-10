0f2f1dca86b0884b7124e70c4d939532ea15f8f2
Elliott Cost <elliottcost@gmail.com>
2019-12-10 13:17:53 -0500

    removed limit on nav pages and refactored cms index

--- site/snippets/nav.php
+++ site/snippets/nav.php
@@ -1,7 +1,7 @@
 <?php if ($page->pause_site() == "true"): ?>
 <?php else: ?>
 <nav>
-<?php foreach($site->children()->listed()->filterBy('template', 'in', ['home', 'default'])->limit(8) as $item): ?>
+<?php foreach($site->children()->listed()->filterBy('template', 'in', ['home', 'default']) as $item): ?>
     <a <?php e($item->isActive(), ' class="active"') ?> href="<?php echo $item->url() ?>"></a>
 <?php endforeach ?>
 
